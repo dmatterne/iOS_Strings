e496825f9fce37e29e218f90b82614be0691a687
David Matterne <david.matterne@gmail.com>
2016-09-26 13:21:21 +0200

    Initial Commit

---
+++ Strings/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Strings
+//
+//  Created by Stannis Baratheon on 26/09/16.
+//  Copyright © 2016 Training. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
